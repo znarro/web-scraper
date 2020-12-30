0c3cd92e8594b583dda7cdb2bd9e8ae2b7308401
Josue Narro <josue.narro.a@gmail.com>
2020-12-30 17:33:37 -0500

    Create model and install Django rest framework

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class ProyectoDeLey(models.Model):
+    numero = models.CharField(max_length=50, unique=True)
+    fec_ult = models.CharField(max_length=20, blank=True, null=True)
+    fec_pres = models.CharField(max_length=20)
+    estado = models.CharField(max_length=100)
+    titulo_del_proyecto = models.TextField()
+
+    def __str__(self):
+        return self.titulo_del_proyecto
--- backend/web_scraper_project/settings.py
+++ backend/web_scraper_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third party
+    "rest_framework",
     # Local
     "api.apps.ApiConfig",
 ]
@@ -120,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ]
+}
