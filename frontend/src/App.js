33b5b45dbe7a46d19474b82c77a54917132aa6da
Josue Narro <josue.narro.a@gmail.com>
2020-12-30 23:22:24 -0500

    Refactor api call function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,15 @@
 import React, { useState, useEffect } from "react"
 import { VStack, Heading } from "@chakra-ui/react"
-import axios from "axios"
 import TablaDeProyectos from "./components/TablaDeProyectos"
+import getProyectosFromAPI from "./calls/api"
+
+const API_URL = "http://localhost:8000/api/proyectos-de-ley/"
 
 function App() {
   const [proyectos, setProyectos] = useState([])
 
   useEffect(() => {
-    const API_URL = "http://localhost:8000/api/proyectos-de-ley/"
-
-    async function getProyectosFromAPI() {
-      const response = await axios.get(API_URL)
-      setProyectos(response.data)
-    }
-
-    getProyectosFromAPI()
+    getProyectosFromAPI(API_URL, setProyectos)
   }, [])
 
   return (
