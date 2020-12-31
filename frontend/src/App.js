4fa0be826b48d64f8b684a1fd14429cf5048a4e3
Josue Narro <josue.narro.a@gmail.com>
2020-12-30 20:22:33 -0500

    Config Eslint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,29 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
 function App() {
-  const [proyectos, setProyectos] = useState([]);
+  const [proyectos, setProyectos] = useState([])
 
   useEffect(() => {
-    const API_URL = "http://localhost:8000/api/proyectos-de-ley/";
+    const API_URL = "http://localhost:8000/api/proyectos-de-ley/"
 
     async function getProyectosFromAPI() {
-      const response = await axios.get(API_URL);
-      setProyectos(response.data);
+      const response = await axios.get(API_URL)
+      setProyectos(response.data)
     }
 
-    getProyectosFromAPI();
-  }, []);
+    getProyectosFromAPI()
+  }, [])
 
   return (
     <div>
       <h1>Proyectos de Ley</h1>
 
-      {proyectos.map((proyecto) => {
-        return <p>{proyecto["numero"]}</p>;
-      })}
+      {proyectos.map((proyecto) => (
+        <p>{proyecto.numero}</p>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
