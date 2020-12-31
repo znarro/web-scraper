791f4fa13be498d59cf6624e88df26de17b099e2
Josue Narro <josue.narro.a@gmail.com>
2020-12-30 19:52:37 -0500

    Install axios and consume API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,28 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
-  return <div></div>;
+  const [proyectos, setProyectos] = useState([]);
+
+  useEffect(() => {
+    const API_URL = "http://localhost:8000/api/proyectos-de-ley/";
+
+    async function getProyectosFromAPI() {
+      const response = await axios.get(API_URL);
+      setProyectos(response.data);
+    }
+
+    getProyectosFromAPI();
+  }, [proyectos]);
+
+  return (
+    <div>
+      <h1>Proyectos de Ley</h1>
+      {proyectos.map((proyecto) => {
+        return <p>{proyecto["numero"]}</p>;
+      })}
+    </div>
+  );
 }
 
 export default App;
